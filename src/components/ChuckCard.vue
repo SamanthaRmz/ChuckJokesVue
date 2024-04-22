6d9df99fcf9f8ad29071794963ba069ee69d4340
Samantha Ramirez <samantharamirez@MacBook-Air-de-Samantha.local>
2024-04-21 23:56:20 -0500

    Tarea 10

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+    <div class="card-body">
+      <div class="text-area">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    iconUrl: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.text-area {
+  background-color: #5CD18B;
+  padding: 10px;
+  margin-bottom: 25px;
+}
+</style>
--- src/components/ChuckNorrisFacts.vue
+++ src/components/ChuckNorrisFacts.vue
@@ -1,26 +1,60 @@
 <template>
   <div class="container">
     <h1 class="title">Hechos de Chuck Norris</h1>
-    <ul class="fact-list">
-      <li class="fact-item" v-for="(fact, index) in chuck" :key="index">{{ fact.value }}</li>
-    </ul>
+    <div class="fact-list">
+      <div class="row">
+        <div class="col-md-4 col-6" v-for="(fact, index) in chuck.slice(0, 3)" :key="index">
+          <ChuckCard :icon-url="fact.icon_url" :value="fact.value" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-6" v-for="(fact, index) in chuck.slice(3, 6)" :key="index">
+          <ChuckCard :icon-url="fact.icon_url" :value="fact.value" />
+        </div>
+      </div>
+      <!-- Agregar más filas si es necesario -->
+    </div>
   </div>
 </template>
 
 <script>
+import ChuckCard from './ChuckCard.vue';
+
 export default {
+  components: {
+    ChuckCard
+  },
   data() {
     return {
       chuck: [
-        { value: "Chuck Norris can skydive into outer space." },
-        { value: "The chief export of Chuck Norris is pain." },
-        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-        { value: "Time waits for no man. Unless that man is Chuck Norris." },
-        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Chuck Norris can skydive into outer space." },
+        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "The chief export of Chuck Norris is pain." },
+        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
       ]
     };
   }
 };
 </script>
 
-<style src="./ChuckNorrisFacts.css"></style>
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0px;
+}
+
+.title {
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 35px;
+}
+
+.fact-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
